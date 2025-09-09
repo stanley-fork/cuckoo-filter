09a5f3360d317a434b24c9da9d678867ad7fd968
Tim Dortmann <git@tdortman.com>
2025-09-09 19:50:08 +0200

    mogon jobscript

---
+++ mogon_jobscript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -J Cuckoo-Filter    # Job name
+#SBATCH -o \%x_\%j.out      # Specify stdout output file where \%j expands to jobID and \%x to JobName
+#SBATCH -A ki-acccomp       # Account name
+#SBATCH -p a100dl           # Queue name
+#SBATCH -n 1                # Number of tasks
+#SBATCH -c 8                # Number of CPUs
+#SBATCH --gres=gpu:1        # Total number of GPUs
+#SBATCH --mem=10G           # Memory per node
+#SBATCH -t 3                # Time in minutes
+
+set -e
+
+module purge
+module load tools/Meson/1.4.0-GCCcore-13.3.0
+module use /apps/easybuild/2024/cuda/modules/all
+module load tools/Ninja
+module load system/CUDA
+
+srun meson setup build
+srun ninja -C build
+
+srun ./build/cuckoo-filter-cuda
