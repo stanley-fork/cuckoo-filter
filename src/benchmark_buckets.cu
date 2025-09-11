df2caecc5f399e4af7010eea2f9f839c5447ddc9
Tim Dortmann <git@tdortman.com>
2025-09-11 12:37:53 +0200

    append to csv file instead

--- src/benchmark_buckets.cu
+++ src/benchmark_buckets.cu
@@ -6,6 +6,7 @@
 #include <iomanip>
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <random>
 #include <string>
 #include <vector>
@@ -44,90 +45,91 @@ struct BucketSizeBenchmarkResult {
 };
 
 template <typename Func>
-std::vector<double> benchmarkFunction(Func func, int iterations = 5) {
+std::vector<double> benchmarkFunction(Func func, int iterations = 3) {
     std::vector<double> times(iterations);
-
     for (int i = 0; i < iterations; ++i) {
         auto start = std::chrono::high_resolution_clock::now();
         func();
         auto end = std::chrono::high_resolution_clock::now();
-
         auto duration =
             std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
         double timeMs = static_cast<double>(duration.count()) / 1000.0 / 1000.0;
         times[i] = timeMs;
     }
-
     return times;
 }
 
 template <size_t bucketSize>
 BucketSizeBenchmarkResult
 benchmarkCpuTableWithBucketSize(uint32_t* input, size_t n, int exponent) {
+    const int num_runs = 3;
     BucketSizeBenchmarkResult result;
     result.exponent = exponent;
     result.n = n;
     result.bucketSize = bucketSize;
     result.tableType = "BucketsTableCpu";
 
-    size_t count = 0;
-    size_t found = 0;
-    double insertTime = 0.0;
-    double queryTime = 0.0;
+    double totalInsertTime = 0.0;
+    double totalQueryTime = 0.0;
+    size_t totalItemsInserted = 0;
+    size_t totalItemsFound = 0;
+    double totalLoadFactor = 0.0;
 
     auto benchmarkFunc = [&]() {
         auto table =
             BucketsTableCpu<uint32_t, 32, bucketSize, 1000>(n / bucketSize);
 
         auto insert_start = std::chrono::high_resolution_clock::now();
+        size_t current_inserts = 0;
         for (size_t i = 0; i < n; ++i) {
-            count += size_t(table.insert(input[i]));
+            if (table.insert(input[i])) {
+                current_inserts++;
+            }
         }
         auto insert_end = std::chrono::high_resolution_clock::now();
-        insertTime = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                         insert_end - insert_start
-                     )
-                         .count() /
-                     1000.0 / 1000.0;
+        totalInsertTime += std::chrono::duration_cast<std::chrono::nanoseconds>(
+                               insert_end - insert_start
+                           )
+                               .count() /
+                           1000.0 / 1000.0;
+        totalItemsInserted += current_inserts;
 
         auto query_start = std::chrono::high_resolution_clock::now();
         bool* output = table.containsMany(input, n);
         auto query_end = std::chrono::high_resolution_clock::now();
-        queryTime = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                        query_end - query_start
-                    )
-                        .count() /
-                    1000.0 / 1000.0;
+        totalQueryTime += std::chrono::duration_cast<std::chrono::nanoseconds>(
+                              query_end - query_start
+                          )
+                              .count() /
+                          1000.0 / 1000.0;
 
-        found = countOnes(output, n);
+        totalItemsFound += countOnes(output, n);
         std::free(output);
 
-        result.loadFactor = table.loadFactor();
+        totalLoadFactor += table.loadFactor();
         result.falsePositiveRate = table.expectedFalsePositiveRate();
         size_t numBuckets = n / bucketSize;
         result.memoryUsageBytes = numBuckets * (bucketSize * sizeof(uint32_t) +
                                                 sizeof(int) + sizeof(size_t));
     };
 
-    auto times = benchmarkFunction(benchmarkFunc);
+    auto times = benchmarkFunction(benchmarkFunc, num_runs);
 
-    result.itemsInserted = count;
-    result.itemsFound = found;
-    result.avgsInsertTimeMs = insertTime;
-    result.avgQueryTimeMs = queryTime;
+    result.avgsInsertTimeMs = totalInsertTime / num_runs;
+    result.avgQueryTimeMs = totalQueryTime / num_runs;
+    result.itemsInserted = totalItemsInserted / num_runs;
+    result.itemsFound = totalItemsFound / num_runs;
+    result.loadFactor = totalLoadFactor / num_runs;
+
     result.minTotalTimeMs = *std::min_element(times.begin(), times.end());
     result.maxTotalTimeMs = *std::max_element(times.begin(), times.end());
+    result.avgTotalTimeMs =
+        std::accumulate(times.begin(), times.end(), 0.0) / times.size();
 
-    double sum = 0.0;
-    for (double time : times) {
-        sum += time;
-    }
-    result.avgTotalTimeMs = sum / times.size();
-
-    result.insertThroughputMops =
-        (static_cast<double>(n) / 1'000'000.0) / (insertTime / 1'000.0);
+    result.insertThroughputMops = (static_cast<double>(n) / 1000000.0) /
+                                  (result.avgsInsertTimeMs / 1000.0);
     result.queryThroughputMops =
-        (static_cast<double>(n) / 1'000'000.0) / (queryTime / 1'000.0);
+        (static_cast<double>(n) / 1000000.0) / (result.avgQueryTimeMs / 1000.0);
 
     return result;
 }
@@ -135,141 +137,115 @@ benchmarkCpuTableWithBucketSize(uint32_t* input, size_t n, int exponent) {
 template <size_t bucketSize>
 BucketSizeBenchmarkResult
 benchmarkGpuTableWithBucketSize(uint32_t* input, size_t n, int exponent) {
+    const int num_runs = 3;
     BucketSizeBenchmarkResult result;
     result.exponent = exponent;
     result.n = n;
     result.bucketSize = bucketSize;
     result.tableType = "BucketsTableGpu";
 
-    size_t count = 0;
-    size_t found = 0;
     bool* output = nullptr;
     CUDA_CALL(cudaMallocHost(&output, sizeof(bool) * n));
 
-    double insertTime = 0.0;
-    double queryTime = 0.0;
+    double totalInsertTime = 0.0;
+    double totalQueryTime = 0.0;
+    size_t totalItemsInserted = 0;
+    size_t totalItemsFound = 0;
+    double totalLoadFactor = 0.0;
 
     auto benchmarkFunc = [&]() {
         auto table =
             BucketsTableGpu<uint32_t, 32, bucketSize, 1000>(n / bucketSize);
 
         auto insert_start = std::chrono::high_resolution_clock::now();
-        count = table.insertMany(input, n);
+        size_t count = table.insertMany(input, n);
         auto insert_end = std::chrono::high_resolution_clock::now();
-
-        insertTime = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                         insert_end - insert_start
-                     )
-                         .count() /
-                     1000.0 / 1000.0;
+        totalInsertTime += std::chrono::duration_cast<std::chrono::nanoseconds>(
+                               insert_end - insert_start
+                           )
+                               .count() /
+                           1000.0 / 1000.0;
+        totalItemsInserted += count;
 
         auto query_start = std::chrono::high_resolution_clock::now();
         table.containsMany(input, n, output);
         auto query_end = std::chrono::high_resolution_clock::now();
+        totalQueryTime += std::chrono::duration_cast<std::chrono::nanoseconds>(
+                              query_end - query_start
+                          )
+                              .count() /
+                          1000.0 / 1000.0;
 
-        queryTime = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                        query_end - query_start
-                    )
-                        .count() /
-                    1000.0 / 1000.0;
-
-        found = countOnes(output, n);
-        result.loadFactor = table.loadFactor();
+        totalItemsFound += countOnes(output, n);
+        totalLoadFactor += table.loadFactor();
         result.falsePositiveRate = table.expectedFalsePositiveRate();
-
         size_t numBuckets = n / bucketSize;
         result.memoryUsageBytes = numBuckets * (bucketSize * sizeof(uint32_t) +
                                                 sizeof(int) + sizeof(size_t));
     };
 
-    auto times = benchmarkFunction(benchmarkFunc);
+    auto times = benchmarkFunction(benchmarkFunc, num_runs);
 
-    result.itemsInserted = count;
-    result.itemsFound = found;
-    result.avgsInsertTimeMs = insertTime;
-    result.avgQueryTimeMs = queryTime;
+    result.avgsInsertTimeMs = totalInsertTime / num_runs;
+    result.avgQueryTimeMs = totalQueryTime / num_runs;
+    result.itemsInserted = totalItemsInserted / num_runs;
+    result.itemsFound = totalItemsFound / num_runs;
+    result.loadFactor = totalLoadFactor / num_runs;
+
     result.minTotalTimeMs = *std::min_element(times.begin(), times.end());
     result.maxTotalTimeMs = *std::max_element(times.begin(), times.end());
+    result.avgTotalTimeMs =
+        std::accumulate(times.begin(), times.end(), 0.0) / times.size();
 
-    double sum = 0.0;
-    for (double time : times) {
-        sum += time;
-    }
-    result.avgTotalTimeMs = sum / times.size();
-
-    result.insertThroughputMops =
-        (static_cast<double>(n) / 1000000.0) / (insertTime / 1000.0);
+    result.insertThroughputMops = (static_cast<double>(n) / 1000000.0) /
+                                  (result.avgsInsertTimeMs / 1000.0);
     result.queryThroughputMops =
-        (static_cast<double>(n) / 1000000.0) / (queryTime / 1000.0);
+        (static_cast<double>(n) / 1000000.0) / (result.avgQueryTimeMs / 1000.0);
 
     CUDA_CALL(cudaFreeHost(output));
     return result;
 }
 
-template <typename BenchmarkFunc>
-void runBucketSizeBenchmarks(
-    BenchmarkFunc benchmarkFunc,
-    uint32_t* input,
-    size_t n,
-    int exponent,
-    std::vector<BucketSizeBenchmarkResult>& results
-) {
-    results.push_back(benchmarkFunc.template operator()<4>(input, n, exponent));
-    results.push_back(benchmarkFunc.template operator()<8>(input, n, exponent));
-    results.push_back(
-        benchmarkFunc.template operator()<16>(input, n, exponent)
-    );
-    results.push_back(
-        benchmarkFunc.template operator()<32>(input, n, exponent)
-    );
-    results.push_back(
-        benchmarkFunc.template operator()<64>(input, n, exponent)
-    );
-    results.push_back(
-        benchmarkFunc.template operator()<128>(input, n, exponent)
-    );
-    results.push_back(
-        benchmarkFunc.template operator()<256>(input, n, exponent)
-    );
-}
-
-void writeResultsToCsv(
-    const std::vector<BucketSizeBenchmarkResult>& results,
-    const std::string& filename
-) {
-    std::ofstream file(filename);
+void writeCsvHeader(const std::string& filename) {
+    std::ofstream file(filename, std::ios::trunc);
     if (!file.is_open()) {
         std::cerr << "Error: Could not open file " << filename << " for writing"
                   << std::endl;
         return;
     }
-
     file << "exponent,n,bucketSize,tableType,avgsInsertTimeMs,avgQueryTimeMs,"
          << "avgTotalTimeMs,minTotalTimeMs,maxTotalTimeMs,itemsInserted,"
          << "itemsFound,loadFactor,falsePositiveRate,memoryUsageMB,"
          << "insertThroughputMops,queryThroughputMops\n";
-
-    for (const auto& result : results) {
-        file << result.exponent << "," << result.n << "," << result.bucketSize
-             << "," << result.tableType << "," << std::fixed
-             << std::setprecision(6) << result.avgsInsertTimeMs << ","
-             << std::fixed << std::setprecision(6) << result.avgQueryTimeMs
-             << "," << std::fixed << std::setprecision(6)
-             << result.avgTotalTimeMs << "," << std::fixed
-             << std::setprecision(6) << result.minTotalTimeMs << ","
-             << std::fixed << std::setprecision(6) << result.maxTotalTimeMs
-             << "," << result.itemsInserted << "," << result.itemsFound << ","
-             << std::fixed << std::setprecision(4) << result.loadFactor << ","
-             << std::fixed << std::setprecision(8) << result.falsePositiveRate
-             << "," << std::fixed << std::setprecision(2)
-             << (result.memoryUsageBytes / (1024.0 * 1024.0)) << ","
-             << std::fixed << std::setprecision(2)
-             << result.insertThroughputMops << "," << std::fixed
-             << std::setprecision(2) << result.queryThroughputMops << "\n";
-    }
-
     file.close();
-    std::cout << "Results written to " << filename << std::endl;
+}
+
+void appendResultToCsv(
+    const BucketSizeBenchmarkResult& result,
+    const std::string& filename
+) {
+    std::ofstream file(filename, std::ios::app);
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << filename
+                  << " for appending" << std::endl;
+        return;
+    }
+    file << result.exponent << "," << result.n << "," << result.bucketSize
+         << "," << result.tableType << "," << std::fixed << std::setprecision(6)
+         << result.avgsInsertTimeMs << "," << std::fixed << std::setprecision(6)
+         << result.avgQueryTimeMs << "," << std::fixed << std::setprecision(6)
+         << result.avgTotalTimeMs << "," << std::fixed << std::setprecision(6)
+         << result.minTotalTimeMs << "," << std::fixed << std::setprecision(6)
+         << result.maxTotalTimeMs << "," << result.itemsInserted << ","
+         << result.itemsFound << "," << std::fixed << std::setprecision(4)
+         << result.loadFactor << "," << std::fixed << std::setprecision(8)
+         << result.falsePositiveRate << "," << std::fixed
+         << std::setprecision(2)
+         << (result.memoryUsageBytes / (1024.0 * 1024.0)) << "," << std::fixed
+         << std::setprecision(2) << result.insertThroughputMops << ","
+         << std::fixed << std::setprecision(2) << result.queryThroughputMops
+         << "\n";
+    file.close();
 }
 
 void printSummaryTable(const std::vector<BucketSizeBenchmarkResult>& results) {
@@ -280,13 +256,13 @@ void printSummaryTable(const std::vector<BucketSizeBenchmarkResult>& results) {
     std::map<
         std::string,
         std::map<size_t, std::vector<BucketSizeBenchmarkResult>>>
-        groupedResults;
+        grouped_results;
 
     for (const auto& result : results) {
-        groupedResults[result.tableType][result.n].push_back(result);
+        grouped_results[result.tableType][result.n].push_back(result);
     }
 
-    for (const auto& [tableType, sizeMap] : groupedResults) {
+    for (const auto& [tableType, size_map] : grouped_results) {
         std::cout << "\n" << tableType << " Performance Analysis:" << std::endl;
         std::cout << std::string(60, '-') << std::endl;
 
@@ -297,7 +273,7 @@ void printSummaryTable(const std::vector<BucketSizeBenchmarkResult>& results) {
                   << std::endl;
         std::cout << std::string(78, '-') << std::endl;
 
-        for (const auto& [data_size, results_vec] : sizeMap) {
+        for (const auto& [data_size, results_vec] : size_map) {
             for (const auto& result : results_vec) {
                 std::cout << std::left << std::setw(12)
                           << ("2^" + std::to_string(result.exponent))
@@ -367,7 +343,11 @@ int main(int argc, char** argv) {
 
     std::vector<BucketSizeBenchmarkResult> results;
 
-    std::cout << "\nRunning bucket size benchmarks..." << std::endl;
+    writeCsvHeader(output_file);
+    std::cout << "Results will be written to " << output_file << std::endl;
+
+    std::cout << "\nRunning bucket size benchmarks (averaging over 3 runs)..."
+              << std::endl;
     std::cout << "Testing bucket sizes: 4, 8, 16, 32, 64, 128, 256"
               << std::endl;
     std::cout << std::string(70, '-') << std::endl;
@@ -379,52 +359,31 @@ int main(int argc, char** argv) {
                   << " elements):" << std::endl;
 
         size_t starting_index = results.size();
+        const int num_bucket_sizes = 7;
+
+        auto run_and_log = [&](auto benchmark_func) {
+            auto result = benchmark_func(input, n, exponent);
+            results.push_back(result);
+            appendResultToCsv(result, output_file);
+        };
 
         // GPU Benchmarks
-        results.push_back(
-            benchmarkGpuTableWithBucketSize<4>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkGpuTableWithBucketSize<8>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkGpuTableWithBucketSize<16>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkGpuTableWithBucketSize<32>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkGpuTableWithBucketSize<64>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkGpuTableWithBucketSize<128>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkGpuTableWithBucketSize<256>(input, n, exponent)
-        );
+        run_and_log(benchmarkGpuTableWithBucketSize<4>);
+        run_and_log(benchmarkGpuTableWithBucketSize<8>);
+        run_and_log(benchmarkGpuTableWithBucketSize<16>);
+        run_and_log(benchmarkGpuTableWithBucketSize<32>);
+        run_and_log(benchmarkGpuTableWithBucketSize<64>);
+        run_and_log(benchmarkGpuTableWithBucketSize<128>);
+        run_and_log(benchmarkGpuTableWithBucketSize<256>);
 
         // CPU Benchmarks
-        results.push_back(
-            benchmarkCpuTableWithBucketSize<4>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkCpuTableWithBucketSize<8>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkCpuTableWithBucketSize<16>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkCpuTableWithBucketSize<32>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkCpuTableWithBucketSize<64>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkCpuTableWithBucketSize<128>(input, n, exponent)
-        );
-        results.push_back(
-            benchmarkCpuTableWithBucketSize<256>(input, n, exponent)
-        );
+        run_and_log(benchmarkCpuTableWithBucketSize<4>);
+        run_and_log(benchmarkCpuTableWithBucketSize<8>);
+        run_and_log(benchmarkCpuTableWithBucketSize<16>);
+        run_and_log(benchmarkCpuTableWithBucketSize<32>);
+        run_and_log(benchmarkCpuTableWithBucketSize<64>);
+        run_and_log(benchmarkCpuTableWithBucketSize<128>);
+        run_and_log(benchmarkCpuTableWithBucketSize<256>);
 
         for (size_t i = starting_index; i < results.size(); ++i) {
             const auto& result = results[i];
@@ -436,13 +395,12 @@ int main(int argc, char** argv) {
                       << std::setw(7) << result.queryThroughputMops
                       << " MOPS (query)" << std::endl;
 
-            if (i == starting_index + 6) {
+            if (i == starting_index + num_bucket_sizes - 1) {
                 std::cout << std::endl;
             }
         }
     }
 
-    writeResultsToCsv(results, output_file);
     printSummaryTable(results);
 
     CUDA_CALL(cudaFreeHost(input));
