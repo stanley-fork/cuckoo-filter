13585feb151889de3399906f10a4d1b58861a738
Tim Dortmann <git@tdortman.com>
2025-12-18 01:12:40 +0100

    http proxy mogon

--- scripts/mogon_env.sh
+++ scripts/mogon_env.sh
@@ -7,3 +7,6 @@ module load tools/Ninja
 module load system/CUDA
 
 export PKG_CONFIG_PATH=/apps/easybuild/cuda/software/NCCL/2.26.6-GCCcore-14.2.0-CUDA-12.8.0/lib/pkgconfig:$PKG_CONFIG_PATH
+
+export http_proxy=http://webproxy.zdv.uni-mainz.de:8888
+export https_proxy=$http_proxy
