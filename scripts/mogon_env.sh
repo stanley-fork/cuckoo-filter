e368a11e3fca5695e2fc14843028234513257a39
Tim Dortmann <git@tdortman.com>
2025-12-18 01:10:07 +0100

    pkg config path

--- scripts/mogon_env.sh
+++ scripts/mogon_env.sh
@@ -5,3 +5,5 @@ module load tools/Meson/1.4.0-GCCcore-13.3.0
 module use /apps/easybuild/2025/cuda/modules/all
 module load tools/Ninja
 module load system/CUDA
+
+export PKG_CONFIG_PATH=/apps/easybuild/cuda/software/NCCL/2.26.6-GCCcore-14.2.0-CUDA-12.8.0/lib/pkgconfig:$PKG_CONFIG_PATH
